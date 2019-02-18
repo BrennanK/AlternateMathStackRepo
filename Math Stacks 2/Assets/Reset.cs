60778b6f2c9f4ce2ceca40a5c8258b8a1a4e2353
Mehmet Holbert <MTHolbert@student.fullsail.edu>
2019-02-18 11:30:29 -0500

    ScreenBounded
    Created screenbounds and fixed exploding box on start issue

---
+++ Math Stacks 2/Assets/Reset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    [SerializeField] private GameObject ResetPoint;
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.GetComponent<NumberGen>())
+        {
+            col.transform.position = ResetPoint.transform.position;
+        }
+    }
+  
+}

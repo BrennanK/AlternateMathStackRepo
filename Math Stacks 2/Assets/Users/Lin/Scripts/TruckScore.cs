32c16c49adf03ed9b8cc768cce1771488ef17949
Lin Qi <>
2019-03-12 16:17:57 -0400

    High score camera change

--- Math Stacks 2/Assets/Users/Lin/Scripts/TruckScore.cs
+++ Math Stacks 2/Assets/Users/Lin/Scripts/TruckScore.cs
@@ -9,17 +9,20 @@ public class TruckScore : MonoBehaviour
     public GameObject truck;
 
     private bool change = true;
+    public Camera viewCamera;
 
     public List<Text> scoreBoards;
     // Start is called before the first frame update
     void Start()
     {
         truck = this.gameObject;
+        viewCamera = Camera.current;
     }
 
     // Update is called once per frame
     void Update()
     {
+        viewCamera = Camera.current;
         /*if (change)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -38,15 +41,17 @@ public class TruckScore : MonoBehaviour
     }
     public void TruckChangePosition()
     {
-        truck.transform.localPosition = new Vector3(5f, 1.48f, 4f);
+        truck.transform.localPosition = new Vector3(4.5f, 1.8f, -10.5f);
         //Quaternion target = Quaternion.Euler(0, 180, 0);
         //truck.transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime*1f);
         truck.transform.Rotate(0,180,0);
+        viewCamera.GetComponent<Camera>().orthographicSize = 2.75f;
     }
 
     public void TruckChangeBack()
     {
         truck.transform.Rotate(0, 180, 0);
         truck.transform.localPosition = new Vector3(- 3.08f, 1.48f, 4f);
+        viewCamera.GetComponent<Camera>().orthographicSize = 4f;
     }
 }

2b5e012eaa3307e006ded4c1d22922bca7d95b6e
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-03-13 09:10:03 -0400

    add the high score in score.script

--- Math Stacks 2/Assets/Users/Antonio/Scripts/Score.cs
+++ Math Stacks 2/Assets/Users/Antonio/Scripts/Score.cs
@@ -16,7 +16,7 @@ public class Score : MonoBehaviour
     private GameManager GM;
 
     // Set the values for high scores
-    int currentScore;
+    int currentHighScore;
 
     IList<string> levelList = new List<string>()
     {
@@ -35,7 +35,6 @@ public class Score : MonoBehaviour
 
     private void Start()
     {
-        string temp;
         // Initailize all the keys for the high scores.
         foreach (var level in levelList)
             if (!PlayerPrefs.HasKey(level))
@@ -45,71 +44,8 @@ public class Score : MonoBehaviour
             }
             else
             {
-                Debug.Log("The current high score in level " + level + PlayerPrefs.GetInt(level));
+                Debug.Log("The current high score in " + level + " is " + PlayerPrefs.GetInt(level));
             };
-
-
-
-
-        //if (!PlayerPrefs.HasKey("highScore_K"))
-        //{
-        //    PlayerPrefs.SetInt("highScore_K", 0);
-        //    Debug.Log("Initailize the high score in level K.");
-        //}
-        //else
-        //{
-        //    Debug.Log("The current high score in level K is " + PlayerPrefs.GetInt("highScore_K"));
-        //}
-
-        //if (!PlayerPrefs.HasKey("highScore_1"))
-        //{
-        //    PlayerPrefs.SetInt("highScore_1", 0);
-        //    Debug.Log("Initailize the high score in level 1.");
-        //}
-        //else
-        //{
-        //    Debug.Log("The current high score in level 1 is " + PlayerPrefs.GetInt("highScore_1"));
-        //}
-
-        //if (!PlayerPrefs.HasKey("highScore_2"))
-        //{
-        //    PlayerPrefs.SetInt("highScore_2", 0);
-        //    Debug.Log("Initailize the high score in level 2.");
-        //}
-        //else
-        //{
-        //    Debug.Log("The current high score in level 2 is " + PlayerPrefs.GetInt("highScore_2"));
-        //}
-
-        //if (!PlayerPrefs.HasKey("highScore_3"))
-        //{
-        //    PlayerPrefs.SetInt("highScore_3", 0);
-        //    Debug.Log("Initailize the high score in level 3.");
-        //}
-        //else
-        //{
-        //    Debug.Log("The current high score in level 3 is " + PlayerPrefs.GetInt("highScore_3"));
-        //}
-
-        //if (!PlayerPrefs.HasKey("highScore_4"))
-        //{
-        //    PlayerPrefs.SetInt("highScore_4", 0);
-        //    Debug.Log("Initailize the high score in level 4.");
-        //}
-        //else
-        //{
-        //    Debug.Log("The current high score in level 4 is " + PlayerPrefs.GetInt("highScore_4"));
-        //}
-
-        //if (!PlayerPrefs.HasKey("highScore_5"))
-        //{
-        //    PlayerPrefs.SetInt("highScore_5", 0);
-        //    Debug.Log("Initailize the high score in level 5.");
-        //}
-        //else
-        //{
-        //    Debug.Log("The current high score in level 5 is " + PlayerPrefs.GetInt("highScore_5"));
-        //}
     }
 
     private void Update()
@@ -153,7 +89,9 @@ public class Score : MonoBehaviour
     // Check the highest score and save it.
     public void CheckHighScore()
     {
-        //currentScore = PlayerPrefs.GetInt();
+        currentHighScore = PlayerPrefs.GetInt("highScore_level0");
+
+        
     }
 }
 
